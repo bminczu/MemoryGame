a1cf847fbe335c963eb46fd2e29953b804f8f656
bminczu <bminczu@gmail.com>
2021-07-12 13:45:12 -0500

    cards not flipping atm WIP

--- app.js
+++ app.js
@@ -36,16 +36,94 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/Water.jpeg'
         },
 
-        
+        {
+            name: 'bass',
+            image: 'images/bass.jpeg'
+        } 
     ]
-})
 
-const grid = document.querySelector('.grid')
+    cardArray.sort(() => 0.5 - Math.random())
 
-function createBoard(){
-    for (let i =0, i < cardArray.length; i++){
-        var card = document.createElement("img")
-        card.setAttribute('src', 'images/water.png')
-        card.setAttribute
+    const grid = document.querySelector('.grid')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
+    const resultDisplay = document.querySelector('#result')
+
+
+    
+    function createBoard(){
+        for (let i = 0; i < cardArray.length; i++){
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/water.jpeg')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+    
+
+function checkForMatch(){
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cards[0]
+    const optionTwoId = cardsChosen[1]
+    if (cardsChosen[0] === cardsChosen[1]){
+        alert("You found a match!")
+        cards[optionOneId].setAttribute('src','images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen) 
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        alert("Sorry, try again.")
+    }
+    cardsChosen =[]
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length == cardArray.length/2)
+    resultDisplay.textContent = 'Congratulations! You found them all'
+
+
+}
+
+
+function flipCard(){
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId.name])
+    cardsChosenId.push(CardId)
+    this.setAttribute('src', cardArray[CardId.img])
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
     }
 }
+
+
+
+
+
+
+    createBoard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
